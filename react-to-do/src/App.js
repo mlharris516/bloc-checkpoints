44fc04341b49d16b2fc09de3f449404233997415
meredith <mlharris516@gmail.com>
2019-03-03 07:35:24 -0700

    added props/state in to-do

--- react-to-do/src/App.js
+++ react-to-do/src/App.js
@@ -1,17 +1,30 @@
 import React, { Component } from 'react';
 import './App.css';
 import ToDo from './components/ToDo.js';
+
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      todos: [
+        { description: 'Go to the gym', isCompleted: true },
+        { description: 'Go to work', isCompleted: false },
+        { description: 'Finish to-do assignment', isCompleted: false }
+      ]
+    };
+  }
+  
   render() {
     return (
       <div className="App">
       <ul>
-          <ToDo />
-          <ToDo />
-        </ul> 
-      </div>
-    );
-  }
-}
+      { this.state.todos.map( (todo, index) => 
+            <ToDo key={ index } description={ todo.description } isCompleted={ todo.isCompleted } />
+           )}
+         </ul>
+       </div>
+     );
+   }
+ }
 
 export default App;
